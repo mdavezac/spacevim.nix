e80e0f11f008881722f0bcc193a44f296411dfa9
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2023-01-25 09:30:02 +0000

    Avoid embedded nvims

--- modules/layers/git/config.nix
+++ modules/layers/git/config.nix
@@ -11,20 +11,7 @@ in
     pkgs.vimPlugins.vim-hydra
   ];
   config.nvim.init.vim =
-    let
-      nvr = (
-        "${pkgs.neovim-remote}/bin/nvr " +
-        "--servername $NVIM_LISTEN_ADDRESS " +
-        "-cc split --remote-wait"
-      );
-    in
-    lib.mkIf cfg.nvr
-      ''
-        if has('nvim') && exists("$NVIM_LISTEN_ADDRESS")
-          let $GIT_EDITOR = '${nvr}'
-        endif
-        autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete
-      '';
+    lib.mkIf cfg.nvr ''autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete'';
   config.nvim.init.lua = lib.mkIf enable ''
     require('neogit').setup {
       integrations = { diffview = true },
