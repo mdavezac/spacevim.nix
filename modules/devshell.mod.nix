e21bd609b58c39771b59933fa98d7ef619898938
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2023-01-30 11:27:35 +0000

    Don't use nvr in devshell mod

--- modules/devshell.mod.nix
+++ modules/devshell.mod.nix
@@ -1,30 +1,13 @@
 { pkgs, config, lib, ... }:
 let
-  vicmd = ''
-    if [ -e "$NVIM_LISTEN_ADDRESS" ] && [ ! -z "$NVIM_LISTEN_ADDRESS" ]; then
-       ${pkgs.neovim-remote}/bin/nvr --servername $NVIM_LISTEN_ADDRESS -s $@
-    else
-       echo "Start an nvim process first"
-       exit 1
-    fi
-  '';
-  vi_args = [
-    "${pkgs.neovim-remote}/bin/nvr"
-    "--servername"
-    "$PRJ_DATA_DIR/nvim.rpc"
-    "-cc split"
-    "--remote-wait"
-    "-s"
-    "$@"
-  ];
+  command = ''[ -z "''${NVIM:-}" ] && exec nvim $@ || nvim --server $NVIM --remote $@'';
 in
 {
   imports = [ ./layers ];
   config = {
-    commands = builtins.map (x: { name = x; command = vicmd; help = "Alias to nvim+remote"; }) [ "vim" "vi" ];
+    commands = builtins.map (x: { inherit command; name = x; help = "Alias to nvim+remote"; }) [ "vim" "vi" ];
     env = [
-      { name = "EDITOR"; value = builtins.concatStringsSep " " vi_args; }
-      { name = "NVIM_LISTEN_ADDRESS"; eval = "$PRJ_DATA_DIR/nvim.rpc"; }
+      { name = "EDITOR"; value = "vi"; }
     ];
   };
 }
