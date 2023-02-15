a70bbc085349075767261e1c9b04f29e7cdd3bef
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2023-02-15 21:58:08 +0000

    Remove double header inputs

--- modules/devshell.mod.nix
+++ modules/devshell.mod.nix
@@ -1,13 +1,22 @@
-{ pkgs, config, lib, ... }:
-let
-  command = ''[ -z "''${NVIM:-}" ] && exec nvim $@ || exec nvim --server $NVIM --remote $@'';
-in
 {
-  imports = [ ./layers ];
+  pkgs,
+  config,
+  lib,
+  ...
+}: let
+  command = ''[ -z "''${NVIM:-}" ] && exec nvim $@ || exec nvim --server $NVIM --remote $@'';
+in {
   config = {
-    commands = builtins.map (x: { inherit command; name = x; help = "Alias to nvim+remote"; }) [ "vim" "vi" ];
+    commands = builtins.map (x: {
+      inherit command;
+      name = x;
+      help = "Alias to nvim+remote";
+    }) ["vim" "vi"];
     env = [
-      { name = "EDITOR"; value = "vi"; }
+      {
+        name = "EDITOR";
+        value = "vi";
+      }
     ];
   };
 }
