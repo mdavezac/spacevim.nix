07695939954a6b62b53d2abc11e65a69da3c3966
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2023-07-04 09:09:49 +0100

    updates

--- git/default.nix
+++ git/default.nix
@@ -9,12 +9,12 @@ pkgs.stdenv.mkDerivation {
   installPhase = ''
     mkdir -p $out/share/git/
 
-    sed -e "s!@git@!$out/bin/git!g" ${./gitconfig} > $out/share/git/gitconfig
-    sed -i -e "s!@out@!$out!g" $out/share/git/gitconfig
-    sed -i -e "s!@gh@!${pkgs.gh}/bin/gh!g" $out/share/git/gitconfig
+    sed -e "s!@git@!$out/bin/git!g" ${./gitconfig} > $out/share/git/config
+    sed -i -e "s!@out@!$out!g" $out/share/git/config
+    sed -i -e "s!@gh@!${pkgs.gh}/bin/gh!g" $out/share/git/config
 
-    cp ${./gitignore} $out/share/git/gitignore
+    cp ${./gitignore} $out/share/git/ignore
 
-    makeWrapper ${pkgs.git}/bin/git $out/bin/git  --set-default GIT_CONFIG_GLOBAL "$out/share/git/gitconfig"
+    makeWrapper ${pkgs.git}/bin/git $out/bin/git  --set-default GIT_CONFIG_GLOBAL "$out/share/git/config"
   '';
 }
