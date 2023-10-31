a9612ba69744d1355e4b01383645f4002f71b266
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2023-10-31 08:21:41 +0000

    Update wezterm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,13 +7,13 @@ local config = {}
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.default_prog = { "/Users/mdavezac/.nix-profile/bin/nu", "-l" }
+config.default_prog = { "nu", "-l" }
 config.color_scheme = "Apprentice"
 config.font = wezterm.font("VictorMono Nerd Font")
 config.enable_tab_bar = false
@@ -22,16 +22,16 @@ config.hide_tab_bar_if_only_one_tab = true
 -- In newer versions of wezterm, use the config_builder which will
 -- help provide clearer error messages
 config.keys = {
-  {
-    key = '1',
-    mods = 'ALT',
-    action = wezterm.action.ShowLauncherArgs { flags = 'WORKSPACES' },
-  },
-  {
-    key = '2',
-    mods = 'ALT',
-    action = wezterm.action.ShowLauncherArgs { flags = 'TABS' },
-  },
+	{
+		key = "1",
+		mods = "ALT",
+		action = wezterm.action.ShowLauncherArgs({ flags = "WORKSPACES" }),
+	},
+	{
+		key = "2",
+		mods = "ALT",
+		action = wezterm.action.ShowLauncherArgs({ flags = "TABS" }),
+	},
 }
 
 -- and finally, return the configuration to wezterm
