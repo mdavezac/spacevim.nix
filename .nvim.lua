756c4870e2609e359133863988aed57016ca8b18
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2023-11-18 20:45:49 +0000

    Add catppuccin

--- .nvim.lua
+++ .nvim.lua
@@ -1,8 +1,7 @@
 return {
-  { "uloco/bluloco.nvim", lazy = false, priority = 1000, dependencies = { "rktjmp/lush.nvim" } },
-  {
-    "LazyVim/LazyVim",
-    dir = require("config.directories") .. "/LazyVim",
-    opts = { colorscheme = "bluloco-dark" },
-  },
+	-- { "uloco/bluloco.nvim", lazy = false, priority = 1000, dependencies = { "rktjmp/lush.nvim" } },
+	-- {
+	--   "LazyVim/LazyVim",
+	--   opts = { colorscheme = "bluloco-dark" },
+	-- },
 }
