085c3cc099c291b234516eacf42cf5275bdbc8bc
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2024-03-15 10:56:54 +0000

    Remove mason stuff from cpp

--- spacevim/lua/plugins/cpp.lua
+++ spacevim/lua/plugins/cpp.lua
@@ -103,20 +103,9 @@ return {
 		dependencies = { "alfaix/neotest-gtest" },
 	},
 	{ "alfaix/neotest-gtest" },
-
 	{
 		"mfussenegger/nvim-dap",
 		optional = true,
-		dependencies = {
-			-- Ensure C/C++ debugger is installed
-			"williamboman/mason.nvim",
-			optional = true,
-			opts = function(_, opts)
-				if type(opts.ensure_installed) == "table" then
-					vim.list_extend(opts.ensure_installed, { "codelldb" })
-				end
-			end,
-		},
 		opts = function()
 			local dap = require("dap")
 			if not dap.adapters["codelldb"] then
