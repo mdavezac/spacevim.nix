a045270a43017163663da4770a6fcb5f16ade23f
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2024-05-30 15:22:57 +0100

    Update to neovim 0.10.0

--- spacevim/lua/plugins/rust.lua
+++ spacevim/lua/plugins/rust.lua
@@ -5,11 +5,6 @@ return {
 			{
 				"Saecki/crates.nvim",
 				event = { "BufRead Cargo.toml" },
-				opts = {
-					src = {
-						cmp = { enabled = true },
-					},
-				},
 			},
 		},
 		opts = function(_, opts)
