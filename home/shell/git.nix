b9276c5a9785833db1d798de524e73f5f0ae70f5
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2025-04-24 08:22:06 +0100

    Adding git lfs

--- home/shell/git.nix
+++ home/shell/git.nix
@@ -14,6 +14,7 @@
   };
   programs.git = {
     enable = true;
+    lfs.enable = true;
     aliases.identity = let
       git = "${pkgs.git}/bin/git";
     in ''! ${git} config user.name "''$(${git} config user.''$1.name)" && ${git} config user.email "''$(${git} config user.''$1.email)" && ${git} config user.signingkey "''$(${git} config user.''$1.signingkey)"'';
