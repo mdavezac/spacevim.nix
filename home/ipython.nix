7c8b38fc11124e3c7f1da40c5353d5293d3afd2f
Mayeul d'Avezac <2745737+mdavezac@users.noreply.github.com>
2025-06-18 12:20:12 +0100

    Update ipython inti script

--- home/ipython.nix
+++ home/ipython.nix
@@ -1,7 +1,11 @@
 {
   home.file.".ipython/profile_default/startup/start.ipy".text = ''
+    from pathlib import Path
+    from datetime import datetime, date, timedelta, timezone, UTC
+    from textwrap import dedent
+    from os import environ, getenv
+
     %load_ext autoreload
     %autoreload 2
-    from pathlib import Path
   '';
 }
